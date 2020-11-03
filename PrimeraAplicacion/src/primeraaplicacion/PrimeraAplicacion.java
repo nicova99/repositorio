e7450b0d993d5a5340ab222a13cf49791e018520
alumnado <alumnado@PC12>
2020-11-03 15:23:48 +0100

    Añadido hola mundo

--- PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
+++ PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
@@ -15,6 +15,7 @@ public class PrimeraAplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         // TODO code application logic here
     }
     
